ce0aefd87e805731cd5c8b225602fcb1c2c088b6
Alexsey Shestakov <wingrime@gmail.com>
2011-02-05 01:04:57 +0500

    Удаление Голосового вызова  и добавление файлового менеджера

--- full_swift.mk
+++ full_swift.mk
@@ -24,7 +24,7 @@ DEVICE_PACKAGE_OVERLAYS += device/lg/swift/overlay
     
 PRODUCT_PACKAGES += \
     librs_jni \ 
-    VoiceDialer \
+    FileManager \
     LiveWallpapers \
     LiveWallpapersPicker \
     MagicSmokeWallpapers \
